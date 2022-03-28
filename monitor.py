65f451f1a03ac2f9c4b305b3fd63dba4916709a9
EMANUEL97654 <51054715+EMANUEL97654@users.noreply.github.com>
2022-03-27 21:51:50 -0300

    Add files via upload

---
+++ monitor.py
@@ -0,0 +1,18 @@
+class Monitor:
+    contador_monitores = 0
+
+    def __init__(self,marca,tamanio):
+        Monitor.contador_monitores +=1
+        self._id_monitor = Monitor.contador_monitores
+        self._marca = marca
+        self._tamanio = tamanio
+
+    def __str__(self):
+        return f'Id: {self._id_monitor}, Marca: ´{self._marca}, Tamaño: {self._tamanio}'
+
+
+if __name__ == "__main__":
+    monitor1= Monitor('HP',15)
+    print(monitor1)
+    monitor2 = Monitor('Acer',27)
+    print(monitor2)
